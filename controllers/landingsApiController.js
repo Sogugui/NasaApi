9c32e49e99d8320c62c75f0eddca6438b603a336
Sogugui <sofiamguardia@gmail.com>
2022-08-27 13:01:25 +0200

    add/ querys, routes and controllers: getClass, getYearFrom, getYearTo, getYear, createNewLanding(post), updateLanding(put), dropLandings(delete)

--- controllers/landingsApiController.js
+++ controllers/landingsApiController.js
@@ -1,8 +1,6 @@
 const Landing = require('../models/queryLandings');
 const getLandings = async (req, res) => {
     let minimum_mass=req.query.minimum_mass
-    let mass= req.query.mass
-   
         if(minimum_mass){
             try {
             let landing =  await Landing.getMinimumMass(minimum_mass);// lo que esta entre comillas es para filtrar lo que quiero que me muestre
@@ -15,8 +13,19 @@ const getLandings = async (req, res) => {
            //el .status(200) es para que me devuelva ese numero de codigo de que todo ha ido bien (200) o error(404)
         }
         
-    }else if (mass){try {
-        let especificMass =  await Landing.getMass(mass);// lo que esta entre comillas es para filtrar lo que quiero que me muestre
+    }else if (req.query.id){
+        try {
+        let LandingId =  await Landing.getLandingsById(req.query.id);// lo que esta entre comillas es para filtrar lo que quiero que me muestre
+        res.status(200).json({LandingId});
+     
+    }
+    catch (error) {
+        console.log(`ERROR: ${error.stack}`);
+        res.status(404).json( {"message":"landing por masa especificada no encontrado"});
+    } 
+    }else if (req.query.mass){
+        try {
+        let especificMass =  await Landing.getMass(req.query.mass);// lo que esta entre comillas es para filtrar lo que quiero que me muestre
         res.status(200).json({especificMass});
      
     }
@@ -25,6 +34,46 @@ const getLandings = async (req, res) => {
         res.status(404).json( {"message":"landing por masa especificada no encontrado"});
     } 
     
+    }else if(req.query.recclass){
+    try {
+        let especificClass =  await Landing.getClass(req.query.recclass);// lo que esta entre comillas es para filtrar lo que quiero que me muestre
+        res.status(200).json({especificClass});
+     
+    }
+    catch (error) {
+        console.log(`ERROR: ${error.stack}`);
+        res.status(404).json( {"message":"landing por clase especificada no encontrado"});
+    } 
+    }else if(req.query.from){
+    try {
+        let yearFrom =  await Landing.getYearFrom(req.query.from);// lo que esta entre comillas es para filtrar lo que quiero que me muestre
+        res.status(200).json({yearFrom});
+     
+    }
+    catch (error) {
+        console.log(`ERROR: ${error.stack}`);
+        res.status(404).json( {"message":"landing por yearFrom especificada no encontrado"});
+    } 
+}else if(req.query.to){
+    try {
+        let yearTo =  await Landing.getYearTo(req.query.to);// lo que esta entre comillas es para filtrar lo que quiero que me muestre
+        res.status(200).json({yearTo});
+     
+    }
+    catch (error) {
+        console.log(`ERROR: ${error.stack}`);
+        res.status(404).json( {"message":"landing por yearTo especificada no encontrado"});
+    } 
+}else if(req.query.from&&req.query.to){
+    try {
+        let year =  await Landing.getYear(req.query.from,req.query.to);// lo que esta entre comillas es para filtrar lo que quiero que me muestre
+        res.status(200).json({year});
+     
+    }
+    catch (error) {
+        console.log(`ERROR: ${error.stack}`);
+        res.status(404).json( {"message":"landing por yearTo especificada no encontrado"});
+    } 
 }
  else{
     console.log("aqui!");
@@ -34,23 +83,50 @@ const getLandings = async (req, res) => {
 }
 }
 
-// console.log("aqui estoy!");
-// let landing =  await Landing.getAllLandings()
-// res.status(200).json({landing});
-    
+const postLandings= async (req, res) => {
+    try {
+        let newLanding =  await Landing.createNewLanding(req.body);// lo que esta entre comillas es para filtrar lo que quiero que me muestre
+        res.status(200).json(newLanding);
+        console.log("New landing creado", req.body);
+     
+    }
+    catch (error) {
+        console.log(`ERROR: ${error.stack}`);
+        res.status(404).json( {"message":"New landing no creado"});
+    } 
+}
 
-    //     try {
-    //         let landing= await Landing.find({},'name mass').sort('-mass')//poner las llaves sin nada es que me busque todo y el .sort es para que me ordene los resultados segun la propiedad que yo le pase. el - delante del id indica que el orden sea descendente, si no lo pongo es ascendente
-    //         res.status(200).json({ landing }); 
-    //     }
-    //     catch (error) {
-    //         console.log(`ERROR: ${error.stack}`);
-    //         let landing=[];
-    //         res.status(404).json({"message":"landing no encontrado"});
-            
-    //     }
-    // }
+const putLandings= async (req, res) => {
+    try {
+        let editLanding =  await Landing.updateLanding(req.body);// lo que esta entre comillas es para filtrar lo que quiero que me muestre
+        res.status(200).json(editLanding);
+        console.log("Landing modificado con exito", req.body);
+     
+    }
+    catch (error) {
+        console.log(`ERROR: ${error.stack}`);
+        res.status(404).json( {"message":"Landing no modificado"});
+    } 
+}
+
+const dropLandings= async (req, res) => {
+    try {
+        let dropLanding =  await Landing.dropLandings(req.body);// lo que esta entre comillas es para filtrar lo que quiero que me muestre
+        res.status(200).json(dropLanding);
+        console.log("Landing eliminado con exito", req.body);
+     
+    }
+    catch (error) {
+        console.log(`ERROR: ${error.stack}`);
+        res.status(404).json( {"message":"Landing no eliminado"});
+    } 
+}
+
+
 
 module.exports = {
     getLandings,
+    postLandings,
+    putLandings,
+    dropLandings
     }
--- routes/landingsApiRoutes.js
+++ routes/landingsApiRoutes.js
@@ -7,6 +7,10 @@ const landingsApiController= require("../controllers/landingsApiController");
 const landingsApiRouter= express.Router();
 
 landingsApiRouter.get("/landings",landingsApiController.getLandings)
+landingsApiRouter.post("/create",landingsApiController.postLandings)
+landingsApiRouter.put("/edit",landingsApiController.putLandings)
+landingsApiRouter.delete("/delete",landingsApiController.dropLandings)
+
 
 
 module.exports= landingsApiRouter
